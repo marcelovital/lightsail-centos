15d7f454d8c599824f0eb35c6c0fc2f38f28e0f0
Marcelo Vital <marcelovital@users.noreply.github.com>
2020-12-13 10:59:42 +0000

    CentOS7 keep yum and install docker

--- base-centos7-withkernelupgrade.sh
+++ base-centos7-withkernelupgrade.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-# base-centos7.sh - Originally in https://github.com/marcelovital/lightsail-centos
+# base-centos7-withkernelupgrade.sh - Originally in https://github.com/marcelovital/lightsail-centos
 #
 # This script is intended to take a default CentOS 7 image (tested on "7 1901-01") from AWS , update and tweak it.
+# Includes updated kernel to latest version in ELRepo.
 #
 # USAGE: In AWS Lightsail, create an instance and copy-paste this script into the "Launch Script" box.
 #
@@ -33,6 +34,17 @@ yum install -y yum-utils rpmconf podman buildah skopeo vim git wget ansible bash
 yum upgrade -y | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
 
 echo "============================================================" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+echo "Upgrade to latest Kernel" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+echo "============================================================" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+dnf install -y https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+dnf --enablerepo=elrepo-kernel install -y kernel-ml | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+sudo awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+sudo grub2-set-default 0 | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
+
+echo "============================================================" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
 echo "Cleanup and boot" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
 echo "============================================================" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy.log
 
@@ -41,6 +53,5 @@ yum clean all | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/ce
 mv /home/centos/centos-deploy-started /home/centos/centos-deploy-completed
 cat /etc/centos-release | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy-completed
 systemctl reboot  | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> /home/centos/centos-deploy-completed
-
 
 # Deploy is finished. Remember to "sudo hostnamectl set-hostname your-hostname" and "sudo timedatectl set-timezone your-timezone" after boot
