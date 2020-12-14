6080727c034cbece85bd158ca6a9616c0eef7ca6
Marcelo Vital <marcelovital@users.noreply.github.com>
2020-12-14 15:10:33 +0000

    No git scripts

--- centos8.sh
+++ centos8.sh
@@ -5,38 +5,33 @@ uname -r | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/c
 chown centos:centos /home/centos/centos-deploy*
 chmod 666 /home/centos/centos-deploy*
 
-echo "===== Install git and clone repo =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-yum install -y git | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-git clone https://github.com/marcelovital/lightsail-centos | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-chmod -v +x lightsail-centos/*.sh
-
 echo "===== Update and tweak CentOS 7 =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-~/lightsail-centos/basic-centos7.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/basic-centos7.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 echo "===== Replace YUM by DNF =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-~/lightsail-centos/replace-yum-by-dnf.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/replace-yum-by-dnf.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 echo "===== Upgrading CentOS 7 to CentOS 8 =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-~/lightsail-centos/upgrade-centos7-to-centos8.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/upgrade-centos7-to-centos8.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 echo "===== Install Tools =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-~/lightsail-centos/tools-centos8.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/tools-centos8.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 # ===== UNCOMMENT THE LINES BELOW IF YOU WANT TO INSTALL PODMAN 1.6 =====
 # echo "===== Update Kernel =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-# ~/lightsail-centos/podman1-all.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+# sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/podman1-all.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 # ===== UNCOMMENT THE LINES BELOW IF YOU WANT TO INSTALL PODMAN 2.2 =====
 # echo "===== Update Kernel =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-# ~/lightsail-centos/podman2-centos8.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+# sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/podman2-centos8.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 # ===== UNCOMMENT THE LINES BELOW IF YOU WANT TO INSTALL DOCKER =====
 # echo "===== Update Kernel =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-# ~/lightsail-centos/docker-all.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+# sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/docker-all.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 # ===== UNCOMMENT THE LINES BELOW IF YOU WANT TO UPDATE THE KERNEL =====
 # echo "===== Update Kernel =====" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
-# ~/lightsail-centos/kernelupgrade-centos8.sh | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
+# sh<(https://raw.githubusercontent.com/marcelovital/lightsail-centos/main/kernelupgrade-centos8.sh) | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 
 dnf -y update | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
 dnf clean all | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | tee -a /home/centos/centos-deploy.log
